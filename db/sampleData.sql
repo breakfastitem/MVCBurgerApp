f2a7b44285cfc19247bac6f452a2f7ad3f56791c
breakfastitem <andrew.ehrman@gmail.com>
2021-01-24 16:05:06 -0500

    Added Database init files

---
+++ db/sampleData.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO
+     burgers (burger_name,devoured)
+VALUES
+    ("Burger 1",false),
+    ("Burger 2",false),
+    ("Burger 3",false),
+    ("Burger 4",false),
+    ("Burger 5",true);
