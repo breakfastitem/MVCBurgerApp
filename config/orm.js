d5b88d4265d03da43260bb0bce292ecd04a8e7ef
breakfastitem <andrew.ehrman@gmail.com>
2021-01-25 01:58:22 -0500

    Added callbackfunctions to orm to prevend desync

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,21 @@
 const connection = require("./connection");
 
 function selectAll(){
-    connection.query("SELECT * FROM burgers",(data)=>{
-        if(err)throw err;
-        return data;
+    connection.query("SELECT * FROM burgers",(err,data)=>{
+        callback(err,data);
     });
 }
 
 function insertOne(burgerName,devoured){
-    connection.query("INSERT INTO burgers (burger_name,devoured) VALUES (?,?)",[burgerName,devoured],()=>{
-        if(err)throw err;
+    connection.query("INSERT INTO burgers (burger_name,devoured) VALUES (?,?)",[burgerName,devoured],(err)=>{
+        callback(err);
 
     });
 }
 
 function updateOne(id,burgerName,devoured){
-    connection.query("UPDATE burgers SET burger_name=?,devoured=? WHERE id=?",[burgerName,devoured,id],()=>{
-        if(err)throw err;
-
+    connection.query("UPDATE burgers SET burger_name=?,devoured=? WHERE id=?",[burgerName,devoured,id],(err)=>{
+        callback(err);
     });
 }
 
