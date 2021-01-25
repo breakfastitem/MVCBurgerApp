847547e40d114cf8d6435818115ffae0e4435172
breakfastitem <andrew.ehrman@gmail.com>
2021-01-25 04:38:54 -0500

    Client side script and styling that sends ajax calls.

---
+++ public/js/burger_script.js
@@ -0,0 +1,19 @@
+$("#submit-button").on("click",(event)=>{
+    console.log(event);
+
+    event.preventDefault();
+
+    const burgerName= $("#name-input").val().trim();
+    
+    $.ajax("/api/burgers", {
+        method: "POST",
+        data: {
+            name: burgerName
+        },
+        error: function(err){
+            console.log(err);
+        }
+    }).then((res)=>{
+        console.log(res);
+    });
+});
