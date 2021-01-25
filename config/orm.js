f81ca78fc53f6bb9b3aa4d9f54e918aba40249a9
breakfastitem <andrew.ehrman@gmail.com>
2021-01-25 03:04:20 -0500

    Fixed model and orm to include callback function in parameters

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,19 @@
 const connection = require("./connection");
 
-function selectAll(){
+function selectAll(callback){
     connection.query("SELECT * FROM burgers",(err,data)=>{
         callback(err,data);
     });
 }
 
-function insertOne(burgerName,devoured){
+function insertOne(burgerName,devoured,callback){
     connection.query("INSERT INTO burgers (burger_name,devoured) VALUES (?,?)",[burgerName,devoured],(err)=>{
         callback(err);
 
     });
 }
 
-function updateOne(id,burgerName,devoured){
+function updateOne(id,burgerName,devoured,callback){
     connection.query("UPDATE burgers SET burger_name=?,devoured=? WHERE id=?",[burgerName,devoured,id],(err)=>{
         callback(err);
     });
--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,21 @@ const orm = require("../config/orm");
 
 const burger = {
 
-    addBurger: function (burgerName) {
-        orm.insertOne(burgerName,false);
+    addBurger: function (burgerName,callback) {
+        orm.insertOne(burgerName,false,()=>{
+            callback(err);
+        });
     },
     
-    eatBurger: function(burgerId,burgerName){
-        orm.updateOne(id,burgerName,true);
+    eatBurger: function(burgerId,burgerName,callback){
+        orm.updateOne(id,burgerName,true,(err)=>{
+            callback(err);
+        });
     },
-    selectAllBurgers:function(){
-        return orm.selectAll();
+    selectAllBurgers:function(callback){
+        orm.selectAll((err,data)=>{
+            callback(err,data);
+        });
     }
 }
 
