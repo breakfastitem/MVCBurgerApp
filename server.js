50f9da70ddc4580be781e968f5c8c8d075b7e0e6
breakfastitem <andrew.ehrman@gmail.com>
2021-01-25 03:05:59 -0500

    Created server app and listened to port.

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express =require("express");
+const handlebars = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app =express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.engine("handlebars",handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, ()=>{
+    console.log("http://localhost:" + PORT);
+});
